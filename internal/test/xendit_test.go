5973daba875696a5358f199afe0284317d90066a
Earl <jagbay@agsx.net>
2020-11-04 23:51:05 +0800

    added unit test suite

--- internal/test/xendit_test.go
+++ internal/test/xendit_test.go
@@ -3,20 +3,29 @@ package test
 import (
 	"github.com/johnearl92/xendit-ta.git/internal/handler"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestXenditHandler(t *testing.T) {
+type XenditTestSuite struct {
+	suite.Suite
+	h *handler.XenditHandler
+}
+
+func (suite *XenditTestSuite) TestXenditHealthHandler() {
 	//arrange
 	r, _ := http.NewRequest("GET", "/health", nil)
 	w := httptest.NewRecorder()
 
-	var h *handler.XenditHandler
-	h.GetHeartBeat(w, r)
+	suite.h.GetHeartBeat(w, r)
 
 	resp := w.Result()
-	assert.Equal(t, 200, resp.StatusCode, "expected 200 status code")
+	assert.Equal(suite.T(), 200, resp.StatusCode, "expected 200 status code")
 
+}
+
+func TestXenditTestSuite(t *testing.T) {
+	suite.Run(t, new(XenditTestSuite))
 }
